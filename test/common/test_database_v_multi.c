c7cf240a6f937c3660cd04e02dfccd70a3711323
Ajayswar <ajayswar.s@arm.com>
2025-12-15 15:52:17 +0530

    Update multi test database
    - Added tests common to v1.1 and v1.2
    - Updated test database for multi-option support
    - Commented out v1.0 and v1.2 exclusive tests
    
    Signed-off-by: Ajayswar <ajayswar.s@arm.com>
    Change-Id: I6508f9f8606d460675394cff43df4b4a0df32562

--- test/common/test_database_v_multi.c
+++ test/common/test_database_v_multi.c
@@ -1,11 +1,23 @@
 /*
- * Copyright (c) 2024, Arm Limited or its affiliates. All rights reserved.
+ * Copyright (c) 2024-2026, Arm Limited or its affiliates. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
  */
 
 #include "test_database.h"
+
+/*
+ * Multi-Version Test Database Summary
+ *
+ * This test database includes only those test cases that are common and
+ * compatible between FF-A v1.1 and v1.2. Tests that are exclusive to v1.0
+ * are intentionally omitted.
+ *
+ * The purpose of this database is to verify backward compatibility between
+ * v1.1 and v1.2. Therefore, tests marked as ACS UNVERIFIED are excluded to
+ * maintain reliability and consistency.
+ */
 
 const test_suite_info_t test_suite_list[] = {
     {0, ""},
@@ -25,13 +37,234 @@ const test_db_t test_list[] = {
 
 #if (SUITE == all || SUITE == setup_discovery)
     CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_version),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_features),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_id_get),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_partition_info_get),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_rx_release),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_rxtx_map_and_unmap),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_SETUP_DISCOVERY, rxtx_exclusive_access),
+#endif
+
+#if (SUITE == all || SUITE == direct_messaging)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_DIRECT_MESSAGING, ffa_direct_message_32),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_DIRECT_MESSAGING, ffa_direct_message_64),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_DIRECT_MESSAGING, ffa_direct_message_error),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_DIRECT_MESSAGING, ffa_direct_message_error1),
 #endif
 
 #if (SUITE == all || SUITE == memory_manage)
     CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, ffa_mem_share),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_invalid_handle_tag),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, share_input_error_checks),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, share_state_machine_2),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_state_machine_3),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_state_machine_5),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_rw_retrieve_ro),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_retrieve_align_hint_check),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_relinquish_input_checks),
     CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, ffa_mem_lend),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_invalid_handle_tag),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, lend_input_error_checks),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, lend_input_error_checks2),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_align_hint_check),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_input_checks1),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_input_checks3),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_ro_retrieve_rw),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_rw_retrieve_ro),
     CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, ffa_mem_donate),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, donate_input_error_checks3),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_invalid_handle_tag),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_retrieve_input_checks2),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, relinquish_state_machine_2),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, relinquish_state_machine_5),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, reclaim_input_error_checks),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, reclaim_zero_flag),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_retrieve_input_checks4),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_retrieve_input_checks6),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, lend_input_error_checks3),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_input_checks6),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_state_machine_2),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_state_machine_3),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_state_machine_5),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_state_machine_1),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_state_machine_2),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_state_machine_4),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_retrieve_input_checks3),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_state_machine_5),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_state_machine_3),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_state_machine_6),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_state_machine_4),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_state_machine_1),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, relinquish_state_machine_3),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, relinquish_state_machine_4),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_input_checks9),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_input_checks2),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_state_machine_6),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_state_machine_4),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_state_machine_1),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, share_input_error_checks1),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_multiple_retrievals),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_input_checks5),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, donate_input_error_checks),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_input_checks4),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, donate_input_error_checks2),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_multiple_retrievals),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_retrieve_input_checks1),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_input_checks),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, lend_input_error_checks1),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, share_input_error_checks2),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, donate_input_error_checks4),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_input_checks8),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, lend_input_error_checks4),
+
+#if (PLATFORM_SP_EL == 1)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_lower_upper_boundary_32_spsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_lower_upper_boundary_32_vmvm),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_lower_upper_boundary_64_spsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, donate_lower_upper_boundary_64_vmvm),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, donate_mem_access_after_donate_32_vm),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, donate_mem_access_after_donate_32_sp),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, donate_mem_access_after_donate_64_vm),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, donate_mem_access_after_donate_64_sp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_lower_upper_boundary_32_vmsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_lower_upper_boundary_32_spsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_lower_upper_boundary_32_vmvm),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_lower_upper_boundary_64_vmsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_lower_upper_boundary_64_spsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_lower_upper_boundary_64_vmvm),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, lend_mem_access_after_lend_32_vm),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, lend_mem_access_after_lend_32_sp),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, lend_mem_access_after_lend_64_vm),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, lend_mem_access_after_lend_64_sp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_mem_access_32_vmvm),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_mem_access_32_vmsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_mem_access_32_spsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_mem_access_64_vmvm),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_mem_access_64_vmsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, lend_retrieve_mem_access_64_spsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, relinquish_mem_unmap_check_vmsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, relinquish_mem_unmap_check_vmvm),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, relinquish_mem_unmap_check_spsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_lower_upper_boundary_32_vmsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_lower_upper_boundary_32_spsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_lower_upper_boundary_32_vmvm),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_lower_upper_boundary_64_vmsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_lower_upper_boundary_64_spsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_lower_upper_boundary_64_vmvm),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_ro_retrieve_rw_64_vmsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_ro_retrieve_rw_64_spsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_ro_retrieve_rw_64_vmvm),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_ro_retrieve_rw_32_vmsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_ro_retrieve_rw_32_spsp),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_ro_retrieve_rw_32_vmvm),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, mem_lend_mmio),
 #endif
+#endif
+
+#if (SUITE == all || SUITE == setup_discovery)
+#ifndef TARGET_LINUX
+    CLIENT_SEC_CPU_CLIENT_SERVER_TEST_FN(TESTSUITE_SETUP_DISCOVERY, mp_execution_contexts),
+    CLIENT_SERVER_SEC_CPU_CLIENT_TEST_FN(TESTSUITE_SETUP_DISCOVERY, up_migrate_capable),
+#endif
+    CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_features_intr),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_features_nsbit),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_spm_id_get),
+#endif
+
+#if (SUITE == all || SUITE == direct_messaging)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_DIRECT_MESSAGING, direct_msg_sp_to_vm),
+#endif
+
+#if (SUITE == all || SUITE == indirect_messaging)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INDIRECT_MESSAGING, ffa_msg_send2),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INDIRECT_MESSAGING, ffa_msg_send2_sp),
+#endif
+
+#if (SUITE == all || SUITE == memory_manage)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, mem_security_state_ns_bit),
+#if (PLATFORM_SP_EL == 0)
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, ffa_mem_perm_set),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_MEMORY_MANAGE, ffa_mem_perm_get),
+#endif
+#endif
+
+#if (SUITE == all || SUITE == notifications)
+    CLIENT_TEST_FN_ONLY(TESTSUITE_NOTIFICATIONS, notification_bitmap_create),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_NOTIFICATIONS, notification_bitmap_destroy),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_NOTIFICATIONS, notification_bind),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_NOTIFICATIONS, notification_unbind),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_NOTIFICATIONS, notification_get),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_NOTIFICATIONS, notification_set),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_NOTIFICATIONS, notification_info_get),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_NOTIFICATIONS, notification_comp),
+#if (PLATFORM_SP_EL == 1)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_NOTIFICATIONS, vm_to_sp_notification),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_NOTIFICATIONS, vm_to_sp_notification_pcpu),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_NOTIFICATIONS, sp_signals_vm_sp),
+#endif
+#if (PLATFORM_SP_EL == 0)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_NOTIFICATIONS, vm_to_sp_notification_el0),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_NOTIFICATIONS, vm_to_sp_notification_pcpu_el0),
+#endif
+#endif
+
+
+#ifndef TARGET_LINUX
+#if (SUITE == all || SUITE == interrupts)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, vm_to_sp_preempt),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, ns_intr_queued),
+#if (PLATFORM_SP_EL == 1)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, s_int_sp_preempt),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, other_secure_int1),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, other_secure_int6),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, ns_int_precedence),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, sp_el1_running),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, sp_waiting_el1),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, vm_to_sp_managed_exit),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, vm_to_sp_managed_exit_1),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, vm_to_sp_managed_exit_2),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, vm_to_sp_managed_exit_3),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, sp_to_sp_blocked),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, sp_preempted_el1),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, s_int_ec_blocked),
+#endif
+#if (PLATFORM_SP_EL == 0)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, sp_el0_blocked),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, sp_el0_running),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, sp_waiting_el0),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, sp_preempted_el0),
+#endif
+#endif
+#endif
+
+/* The below test is only in version 1.2 FFA-ACS
+
+#if (SUITE == all || SUITE == setup_discovery)
+    CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_version_negotiation),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_SETUP_DISCOVERY, ffa_msg_wait_rx_buff_rel),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_partition_info_get_regs),
+    CLIENT_TEST_FN_ONLY(TESTSUITE_SETUP_DISCOVERY, ffa_partition_info_get_lsp),
+#endif
+
+#if (SUITE == all || SUITE == memory_manage)
+   CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, mem_share_impdef),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, mem_lend_impdef),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, mem_donate_impdef),
+    CLIENT_SERVER_TEST_FN(TESTSUITE_MEMORY_MANAGE, share_multi_borrower_flag_check),
+#endif
+
+
+
+#if (SUITE == all || SUITE == interrupts)
+#if (PLATFORM_SP_EL == 1)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_INTERRUPTS, sp_el1_int_mask),
+#endif
+#endif
+
+#if (SUITE == all || SUITE == direct_messaging)
+    CLIENT_SERVER_TEST_FN(TESTSUITE_DIRECT_MESSAGING, ffa_direct_message2),
+#endif
+*/
 #endif
 
     {0, "", NULL, NULL, NULL, NULL, NULL},
